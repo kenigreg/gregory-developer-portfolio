1c386e6bd06d8ab6e9e969bd2711773fa3b52940
Gregory Oghenah <kenny@gregorys-mbp.home>
2024-01-27 17:21:27 +0000

    Completed the project setup

---
+++ gregorydeveloperportfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Gregory... Agba developer! Agba Coder!!</h1>
+    </>
+  )
+}
+
+export default App
